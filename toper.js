d8d1cd713c62b57817bc431ef9868b3a3358806c
marcosnoammvp <86367834+marcosnoammvp@users.noreply.github.com>
2021-06-25 02:09:33 +0300

    first commit

---
+++ toper.js
@@ -0,0 +1,15 @@
+function toper () {
+        const iframeContainer = document.createElement("div");
+        const iframe = document.createElement("iframe");
+        iframeContainer.setAttribute("id", "iframeContainer");
+        iframe.setAttribute("id", "iframe");
+        iframe.setAttribute("src", "https://www.commbox.io/he/");
+        const htmlRef = document.querySelector("html");
+        iframeContainer.appendChild(iframe);
+        htmlRef.insertBefore(iframeContainer, htmlRef.firstChild);
+
+        // htmlRef.appendChild(iframeContainer);
+        console.log("Images/Files are fully loaded");
+};
+
+toper();
